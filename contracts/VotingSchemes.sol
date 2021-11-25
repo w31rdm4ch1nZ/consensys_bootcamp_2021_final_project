bfdf2e2e1726adcc183ca9b7eaa36c6ceae87d13
weirdmachines.dysfunctional <w31rdm4ch1n3z@protonmail.com>
2021-11-25 14:44:54 -0500

    simplest voting mechanism in VotingSchemes contract - should change that name :)

--- contracts/VotingSchemes.sol
+++ contracts/VotingSchemes.sol
@@ -5,6 +5,7 @@
 
 */
 
+/*
 contract Vote {
     //TO DO
 
@@ -24,3 +25,187 @@ contract Vote {
 
 
 }
+*/
+
+//Actual cimple vote implementation:
+// https://www.toshblocks.com/solidity/complete-example-voting-ballot-smart-contract/
+
+
+//(you now have to link it in some vote cases -adding a function- 
+// to a staking position that will modify almost all parameters and variables, like weight, a read() 
+// function in this contract on the FundsManager contract, etc. )
+
+//check this contract for outdated coding patterns (like will you use "throw here"?)
+pragma solidity ^0.8.0;
+
+/// @title Voting with delegation.
+contract VotingSchemes {
+    // This declares a new complex type which will
+    // be used for variables later.
+    // It will represent a single voter.
+    struct Voter {
+        uint weight; // weight is accumulated by delegation
+        bool voted;  // if true, that person already voted
+        address delegate; // person delegated to
+        uint vote;   // index of the voted proposal
+    }
+
+    // This is a type for a single proposal.
+    struct Proposal
+    {
+        bytes32 name;   // short name (up to 32 bytes)
+        uint voteCount; // number of accumulated votes
+    }
+
+    address public chairperson;
+
+    // This declares a state variable that
+    // stores a `Voter` struct for each possible address.
+    mapping(address => Voter) public voters;
+
+    // A dynamically-sized array of `Proposal` structs.
+    Proposal[] public proposals;
+
+    /// Create a new ballot to choose one of `proposalNames`.
+    function Ballot(bytes32[] proposalNames) {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+
+        // For each of the provided proposal names,
+        // create a new proposal object and add it
+        // to the end of the array.
+        for (uint i = 0; i < proposalNames.length; i++) {
+            // `Proposal({...})` creates a temporary
+            // Proposal object and `proposals.push(...)`
+            // appends it to the end of `proposals`.
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
+        }
+    }
+
+    // Give `voter` the right to vote on this ballot.
+    // May only be called by `chairperson`.
+    function giveRightToVote(address voter) {
+        if (msg.sender != chairperson || voters[voter].voted) {
+            // `throw` terminates and reverts all changes to
+            // the state and to Ether balances. It is often
+            // a good idea to use this if functions are
+            // called incorrectly. But watch out, this
+            // will also consume all provided gas.
+            throw;
+        }
+        voters[voter].weight = 1;
+    }
+
+    /// Delegate your vote to the voter `to`.
+    function delegate(address to) {
+        // assigns reference
+        Voter sender = voters[msg.sender];
+        if (sender.voted)
+            throw;
+
+        // Forward the delegation as long as
+        // `to` also delegated.
+        // In general, such loops are very dangerous,
+        // because if they run too long, they might
+        // need more gas than is available in a block.
+        // In this case, the delegation will not be executed,
+        // but in other situations, such loops might
+        // cause a contract to get "stuck" completely.
+        while (
+            voters[to].delegate != address(0) &&
+            voters[to].delegate != msg.sender
+        ) {
+            to = voters[to].delegate;
+        }
+
+        // We found a loop in the delegation, not allowed.
+        if (to == msg.sender) {
+            throw;
+        }
+
+        // Since `sender` is a reference, this
+        // modifies `voters[msg.sender].voted`
+        sender.voted = true;
+        sender.delegate = to;
+        Voter delegate = voters[to];
+        if (delegate.voted) {
+            // If the delegate already voted,
+            // directly add to the number of votes
+            proposals[delegate.vote].voteCount += sender.weight;
+        } else {
+            // If the delegate did not vote yet,
+            // add to her weight.
+            delegate.weight += sender.weight;
+        }
+    }
+
+    /// Give your vote (including votes delegated to you)
+    /// to proposal `proposals[proposal].name`.
+    function vote(uint proposal) {
+        Voter sender = voters[msg.sender];
+        if (sender.voted)
+            throw;
+        sender.voted = true;
+        sender.vote = proposal;
+
+        // If `proposal` is out of the range of the array,
+        // this will throw automatically and revert all
+        // changes.
+        proposals[proposal].voteCount += sender.weight;
+    }
+
+    /// @dev Computes the winning proposal taking all
+    /// previous votes into account.
+    function winningProposal() constant
+            returns (uint winningProposal)
+    {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal = p;
+            }
+        }
+    }
+
+    // Calls winningProposal() function to get the index
+    // of the winner contained in the proposals array and then
+    // returns the name of the winner
+    function winnerName() constant
+            returns (bytes32 winnerName)
+    {
+        winnerName = proposals[winningProposal()].name;
+    }
+
+    //vote functions to add for staking-backed votes cases, in you use case there are some.
+    // But the first check through giveRightToVote() is doing
+    // most of the work already. What's left is how the stake of an account is going to affect a vote weight calculation, how the output of the vote
+    // is going to be usable as an input for rewards and/or slashing of staked funds. 
+
+    //>>>>>DON'T FORGET: think of that directly related to the "yes/no" 2 ERC-20 tokens minted at the mint of your ERC-1155 RfC token!!!!<<<<<
+
+    //So, now, the scaffold of those few functions and tweaks to add to this contract:
+
+    //1. call a getStakedFunds() (or commitedFunds, whatever the name) of the FundsManager contract and keep in memory for the time of the vote
+    // the output
+    function getStakedFundsOnVoteTime() public view returns(uint256 stakedAmount) {
+        //TO DO: write this or invoke it on a public state variable in FundsManager contract
+        _getFromFundsManager();
+    }
+    
+    //2.1. Then overwrite/rewrite function giveRightToVote(address voter)(or any safe pattern - so it can't be called when you dont want it to be called - likely, you'll rewrite this function so
+    // it encodes both voters[voter].weight = 1, and the version where weight is indexed on the voters' staked funds in FundsManager contract):
+    function giveRightToVote(address voter) internal onlyStakers returns(uint256 ) {
+        //TO DO
+    }
+
+    //2.2. Same case added to the function delegate(address _addr)
+
+    //3. Make those functions callable on a wide set of voters(-stakers)
+
+    
+}
+
