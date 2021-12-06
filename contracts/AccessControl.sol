dc50323bfcc1323286dbb725f92a3d92539c7f43
weirdmachines.dysfunctional <w31rdm4ch1n3z@protonmail.com>
2021-12-06 13:03:31 -0500

    more precise scaffolding of FundsManager logic and features to actually be implemented (TBC)

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-only
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
 //Define the roles and authorizations (mostly through modifiers - so maybe an abstract contract inherited 
--- contracts/FundsManager.sol
+++ contracts/FundsManager.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-//the abicoder-v2, to allow for arbitrary nested arrays and structs to be encoded and decoded in calldata. => to research.
-//pragma abicoder v2;
-
-/* [[Check the design as the ERC1155 inehirted contract should bear most of the heavy lifting, including all minting. 
-    Maybe only when RfC comes to be minted, then it takes full control over the mechanisms]]
-*/
+//MAYBE NO UPSIDE TO PROCESS SEPARATELY CPs AND  INVESTORs FUNDS => same cDAI for all committed participants, but issuance of bond-like tokens that allows
+// to further differentiate uses. 
 
 //This contract acts like a bank (more than an escrow) where clients are investors and CPs, on steroids because using crypto money markets for yields.
 contract FundsManager {
@@ -14,20 +10,27 @@ contract FundsManager {
     //set of amount states value necessary for our various use cases that will be used as input to mint our RfC 1155 tokens
     // (ex: minting an NFT and somr ERC-20 and sending both to a user for an art project that wants to give back to the investors
     // through the artwork + some of the benefits made during the drop, or after; ):
-    uint256 public investorBalance;
-    uint256 public investorTotalPooledFunds;    // defined only for 1 RfC for now (might rethink later the mechanics so the logic would be extended over different 
-                                                // pools for a single investor - more capital efficient for each investor (like pooled txs, compounded rewards,
-                                                // lower fees, etc.))
+    
+    address public immutable testnetWETH = "";   //hardcoding for now the eth erc20 wrapper contract adress on the testnet used
 
-    //for all individual partcipant account state tracking:
-    address public protocolParticipant;
+    address public account;
+
+    mapping (address => uint256) balances;
 
     //1st, simple, deposit to the contract (also an event):
-    //to "get" the addresses of users with deposits in the contract:
+    //to "get" the addresses of users with deposits in the contract. Used also to have a list of users addr using the protocol:
     mapping (address => bool) public userHaveFundsDeposited;
 
-    //
+    //set as soon as funds are sent from the account address for all participants. To be used for a modifier or authorization role
+    mapping (address => bool) isProtocolParticipant;
 
+    //Either this or the following struct to distinguish between differerent types of users
+    mapping (address => bool) isInvestor;
+    mapping (address => bool) isContentProvider;
+    mapping (address => bool) isContentProviderDelegatee;   // allows less users to access possibly sensitive functions (a subset of the CPs functions and actions)
+    mapping (address => bool) isContentConsumer;
+
+    /* obsolete for now => I can't really say why I would choose one over the other option... 
     //to distinguish among the participants between our 3 types of participant (all users will be qualified based on tghe transactions conducted, implying 
     // some funds commitment - so that it is safe to assume they are Investor, or simple Consumer)
     struct UserType{
@@ -35,29 +38,39 @@ contract FundsManager {
         address ContentProvider;
         address ContentConsumer;
     }
+    */
 
-    mapping (address => uint256) balances;
+    //used to differentiate accounts linked to a specific RfC token so we can pool those funds together
+    mapping (address => uint) accountRfCIdCommited;
+
+    //output from the pooling funds function TBD:
+    uint256 public investorTotalPooledFunds;    // defined only for 1 RfC for now (might rethink later the mechanics so the logic would be extended over different 
+                                                // pools for a single investor - more capital efficient for each investor (like pooled txs, compounded rewards,
+                                                // lower fees, etc.))
 
     //instantiation of the RfC contract to use it in relation to the fund management logic (rewards payment, splitted RfC payment calculation
     // and distribution, backing RfC with ERC20, etc.)
     RequestForContent public RfC;           // maybe call throuhgh an interface (making it easier to upgrade and callable by other contracts: 
                                             // IRequestForContent public immutable RfC)
 
+    //maybe defined in an interface for RfC tokens:
+    //uint RfCId;
+
     //MODIFIERS
 
     // Those three will enable a control over the feature accessed by our different type of users, both for use and security reasons:
     modifier isInvestor{
-        require(protocolParticipant == UserType.Investor, "It requires a user being an 'investor' to make this transaction");
+        require(isInvestor == false, "It requires a user being an 'investor' to make this transaction");
         _;
     }
 
     modifier isContentProvider{
-        require(protocolParticipant == UserType.ContentProvider, "It requires a user to be a 'Content Provider' to make this transaction");
+        require(isContentProvider == false, "It requires a user to be a 'Content Provider' to make this transaction");
         _;
     }
 
     modifier isContentConsumer{
-        require(protocolParticipant == UserType.ContentConsumer, "It requires a user to be a 'Content Consumer' to make this transaction");
+        require(isContentConsumer == false, "It requires a user to be a 'Content Consumer' to make this transaction");
         _;
     }
 
@@ -66,18 +79,22 @@ contract FundsManager {
     
     event FundsReceived(address account);
 
-    //event investorsFundsPooled();
+    event AmountReceivedInEth(uint256 amount);
+
+    event investorsFundsPooled(bool poolingSuccess, uint256 amount);
     
     //event CPsFundsPoooled();
+    //event InvestorsFundsPooled(); => ether swapped for DAI, then deposited for cDAI
 
-    //event FundsSwappedToStablecoin();
+    event FundsSwappedToStablecoin(bool success);
 
 
-    //constructor
+    //constructor definition:
+
 
     /*
     //fallback and ..? functions
-    //Not sure I'll keep this function - should handle every tx sending ethers more gracefully
+    //handle control on conditions for contract receiving the funds: only eth for now (and the usual != 0)
     function () external payable {
         revert();
     }
@@ -87,71 +104,93 @@ contract FundsManager {
     /// @notice a user can read the value of funds held by the contract (before they are put in the "management cycle")
     /// @return The balance of the user
     function getBalance() public view returns (uint) {
-      // 1. A SPECIAL KEYWORD prevents function from editing state variables;
-      //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
       return balances[msg.sender];
     }
 
-    // approval functions for the contract to manage the funds and 1st interact with a wallet (here Metamask) => maybe off-chain, through metamask
+    function getSpecificAccountBalance(address _account) public view returns(uint) {
+        require(_user == isParticipant, "this account is not active in this protocol");
+        return balances[_account];
+    }
+
+    //not necessary but added for clarity, get the contract FundsManager total balance (likely the one before cTokens and any fund used in RfC being processed):
+    function getContractBalance() public view returns(uint) {
+        return balance(address(this));      // address(this) refers to the current contract address (instance deployed)
+    }
+
 
     //TBD: commit scheme to signal interest and intensity of this interest for a RfC by investors
 
     //1. sent funds contract receiving function (the 'commit event' in cases of investors and CPs):
-    function receiveFunds() public payable returns (bool depositSuccess) {
+
+    //Think how to write this logic in a more readable way:
+    function receiveFunds(address _from, uint _amount) public payable {
         //1st function to handle every deposit = before any user's classification, or any swap or tapping in any yield protocol.
         // Simple purpose: receive safely any deposit, and having it ready to be usable (pulled by other more advanced functions)
         userHaveFundsDeposited[msg.sender] = true;
         emit FundsReceived(msg.sender);
+        emit AmountReceivedInEth(msg.value);
 
         /*
         Logic that allows to distinguish between users eventually, but first between the type of transaction asked (all tx return a message to the user for 
         their confirmation so they know what type of tx they commit to):
-            - commit for RfC proposal (-> user gets type investor);
-            - commmit for RfC acceptation (instead of voting, it is a "ready_to_stake scheme", or staging for commit to invest directly in a specific RfC ->
+            (- commit for RfC proposal (-> user gets type investor = not really, particpant is making a proposal, should be kept separately);)
+            - CPs answer with a quote (through the reverse auctions = cf. VoteAndDelegate contract where it is defined - first to accept an amount is winning
+                the RfC "binding agreement" (in code)) => once winner commit a substantial amount of ether, isContentProvider = true;
+            - investors have 2 to 14 days to fund entirely the RfC and this one to be minted;
+                => commmit for RfC acceptation (instead of voting, it is a "ready_to_stake scheme", or staging for commit to invest directly in a specific RfC ->
                 user gets type investor);
-            - commit for minting the RfC ERC1155 token (-> user gets type investor);
+                => accepting more investors once passed the fund amount agreement, as those funds compound yield will be used as an insurance sort of special fund;
+                    => get them smaller shares on the future content (but same redeeming policy in case content is not delivered)   
+            - minting the RfC ERC1155 token (-> user gets type investor) => users' funds allocated get the investor's status (isInvestor);
             - commit for being responsible for the content creation (-> user gets type Content Provider - 1st agree on time to deliver, type/medium of content at
                 delivery, storage requirement of the content => it defines on what specifics the slashing/punishment will happen for the CP if they don't 
                 respect one of the following components/requirements of the request);
-            - commit to handle a subset of the RfC components and requirements attached (-> user gets type Content Provider);
+            - commit to handle a subset of the RfC components and requirements attached (-> user gets type Content Provider delegartee/sub-contractor );
         */
-        
-        return userHaveFundsDeposited[msg.sender];
+
     } 
+
+    //POOLING FUNDS HAS TO BE DONE FOR EACH AND EVERY RfC (so the logic apply sperately and the different outcomes can be processed independently)
+    function poolingFunds(RfC _RfCId) internal returns() {};
 
     //2. swap (all) pooled funds for stablecoin, that has to be supported by Compound atm (for investors and CPs use case - except all cases where instant payment can take place, so no need for swap 
     //  and gas price to happen ) once an RfC is tokenized/minted (adding a modifier RfCTokenMinted()):
         // The question of a protocol native stablecoin is still TBD in the next developments, as it has its rationale, but adds too much complexity rn
     function swapToStablecoin(
-        string _tickerStablecoin,       //only FEI or USDC for now
-        uint _amount, 
+        //uint256 _ether,       //to be swapped for (reduced to eth => control only eth can be sent to the contract to avoid loss of funds and stay simple so far)
+        uint256 _ethAmount, 
         uint _pooledFundsId/* not sure how to implement the pool yet*/
         ) 
-        internal returns(uint amountUSDStablecoin) {
+        internal returns(uint amountDAI) {
 
             //TO DO
 
             //call to Uniswap contract? Check if there is a way to do it all in Compound
+            require(
+            _ether.balanceOf(address(this)) >= _ethAmount,
+            "Core: Not enough ether, or simply wrong currency. We take only ether for now"
+            );
+            
             if (msg.value > 0) {
-                convertEthToExactFEI(paymentAmountInDai);
+                convertEthToExactDAI(paymentAmountInDai);
             } else {
-                require(feiToken.transferFrom(msg.sender, address(this), swapEthAmountInFEI));
+                require(daiToken.transferFrom(msg.sender, address(this), swapEthAmountInDAI));
             }
 
-            //pool the DAI for one specific RfC once this RfC has been accepted, then deposit in Compound:
+            //then deposit in Compound:
 
-            //for local tests - just a simple quotation based on a fixed ether price and only ETH -> FEI
+            //(for local tests - just a simple quotation based on a fixed ether price and only ETH -> DAI)
 
             //event emitted on successful swap
             emit StablecoinswapSuccess();
 
             //pooled capital state updated to reflect the "frozen value"/stablecoin value
 
-            //return this value and the address to 
+            //return this value and the address of the instantiated cToken for this pool
         }
     
-    //function using Uniswapv3 to swap Eth payment (by investors and CPs) to DAI (I'd like FEI if Compound takes it):
+    //function using Uniswapv3 to swap Eth payment (by investors and CPs) to DAI:
     // code taken from (https://soliditydeveloper.com/uniswap3)
     function convertEthToExactDai(uint256 daiAmount) external payable {
         require(daiAmount > 0, "Must pass non 0 DAI amount");
@@ -204,21 +243,18 @@ contract FundsManager {
     }
 
 
-
-
     //3. Call to compound contract to deposit stablecoin and earn on it - which returns cTokens, kept under escrow by this contract 
     //  Take a closer look at the Compound.js wrappers to see if you can do it in the most simple way.
     //      => https://github.com/compound-finance/compound-js
     //          => See in particular: https://compound.finance/docs/compound-js#cToken
     // OR use the Compound (lower-level) API: https://compound.finance/docs/api
     // Also see the Consensys demo github repo: https://github.com/ajb413/consensys-academy-compound-js
-
+    // cf. also FEI Compound call/PCVdeposit contract code
 
     //4. Call to/from RfC contract to makes those pooled cTokens part of the RfC token (maybe using the ownable token interface in proposition state)
-    
-     
+         
 
-    //user-investors functions related to funds management is initiated through the investors' web UI and Metamask confirmation (sig)
+    //user-investors bonds data
     function manageInvestorsFunds(address _sender, uint256 _amount) external payable isInvestor returns() {
         //metadata recording to facilitate the management of the funds of the user-investors,
         // and the tracking of the investors position through an NFT minted
@@ -228,6 +264,7 @@ contract FundsManager {
 
     };
 
+    //CPs bonds-like data to mint one from the RfC contract
     function manageCPsFunds(address _sender, uint256 _amount) external payable isContentProvider returns() {
         //specific metadata reagrding the deposit so the fund management automation can be done properly, 
         // and the tracking of the CP position through an NFT minted
@@ -236,7 +273,6 @@ contract FundsManager {
 
     };
 
-    function poolingFunds() internal returns() {};
 
     //5.1  if RfC delivered successfully,
 
